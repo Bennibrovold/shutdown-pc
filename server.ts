37ac890b33ff9b0436f3cc66d0d5bb110d6677fe
andrey <bennibrovoldcr@gmail.com>
2023-02-21 22:58:10 +1000

    feat: has changed shutdown route and add restart route

--- server.ts
+++ server.ts
@@ -3,19 +3,14 @@ const { exec } = require("child_process");
 const app = express();
 const port = 1000;
 
-app.get("/", (req: any, res: any) => {
-  exec("shutdown /s", (error: any, stdout: any, stderr: any) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
+app.get("/s", (req: any, res: any) => {
+  exec("shutdown /s");
   res.send("Компьютер выключается...");
+});
+
+app.get("/r", (req: any, res: any) => {
+  exec("shutdown /r");
+  res.send("Компьютер перезагружается...");
 });
 
 app.listen(port, () => {
